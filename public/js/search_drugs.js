776aef22b14b3f522694f81dc96bc1b582de28c8
John Boyle <boylejo@flip2.engr.oregonstate.edu>
2022-05-25 20:52:43 -0700

    add search bar to drugs page

---
+++ public/js/search_drugs.js
@@ -0,0 +1,16 @@
+const search_input = document.getElementById("search-drug-generic-name");
+
+search_input.addEventListener("keyup", (e) => {
+
+    const search_value = e.target.value.toLowerCase();
+    const cells = document.querySelectorAll("#drugs-table .generic-name");
+  
+    cells.forEach((cell) => {
+        if(cell.textContent.toLowerCase().includes(search_value)){
+            cell.closest("tr").style.display = "table-row";
+        } else{
+            cell.closest("tr").style.display = "none";
+        }
+    })
+  
+});
